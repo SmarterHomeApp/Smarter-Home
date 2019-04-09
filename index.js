a39bc5996d7581fd8c4735fc5929912672fe2eff
Salim Youssefzadeh <salimyzadeh@gmail.com>
2019-04-09 15:10:40 -0700

    add load type to vantage

--- index.js
+++ index.js
@@ -412,7 +412,7 @@ class VantagePlatform {
 					if (thisItem.LoadType == "Incandescent" || thisItem.LoadType == "Fluor. Mag non-Dim"  || thisItem.LoadType == "Fluor. Magnetic Dim" 
 					 || thisItem.LoadType == "Fluor. Electronic non-Dim"  || thisItem.LoadType == "Fluor. Electronic Dim" 
 					 || thisItem.LoadType == "Magnetic Low Voltage"	 || thisItem.LoadType == "Electronic Low Voltage" || thisItem.LoadType == "Motor" 
-					 || thisItem.LoadType == "Halogen" || thisItem.DeviceCategory == "Lighting") {
+					 || thisItem.LoadType == "Halogen" || thisItem.LoadType == "LED Dim" || thisItem.LoadType == "LED" || thisItem.DeviceCategory == "Lighting") {
 
 						//this.log.warn(sprintf("New light asked (VID=%s, Name=%s, ---)", thisItem.VID, thisItem.Name));
 						if (thisItem.DName !== undefined && thisItem.DName != "" && (typeof thisItem.DName === 'string')) thisItem.Name = thisItem.DName;
